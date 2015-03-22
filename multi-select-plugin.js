a2ac9f32ec7260769865a86fca70757a79b843ed
vijay <vijay@admarvel.com>
2015-03-23 01:01:28 +0530

    stable release

--- multi-select-plugin.js
+++ multi-select-plugin.js
@@ -2,57 +2,106 @@ var createMultiSelectWidget = createMultiSelectWidget || (function(){
   'use strict';
   var pub,
     local = {},
-    info = {}
+    info = {},
+    doc = document
   ;
 
   local = {
     updateList: function() {
-      this.createUnselectedList();
-      this.createSelectedList();
+      this.sort();
+      this.createElementUnselectedList();
+      this.createElementSelectedList();
+      this.checkIfAllSelected();
+    },
+    checkIfAllSelected: function() {
+      if(info.data.unselectedItems.length == 0)
+          info.selectUnselectAll.checked  = true;
+      else
+          info.selectUnselectAll.checked = false;
+    },
+    selectUnselectAll: function() {
+      info.selectUnselectAll.checked ? local.selectAll() : local.unselectAll();
+    },
+    selectAll: function() {
+      while(info.data.unselectedItems.length) {
+        info.data.selectedItems.push(info.data.unselectedItems.pop());
+      }
+      this.updateList();
+    },
+    unselectAll: function() {
+      while(info.data.selectedItems.length) {
+        info.data.unselectedItems.push(info.data.selectedItems.pop());
+      }
+      this.updateList();
     },
     sort: function(){
       info.data.selectedItems.sort();
       info.data.unselectedItems.sort();
     },
-    createUnselectedList: function(){
-      var that = this;
+    createElementUnselectedList: function(){
+      var that = this,
+        frag = doc.createDocumentFragment()
+      ;
       info.unselectedWrapper.innerHTML="";
       info.data.unselectedItems.forEach(function(item){
-        var li = document.createElement("li");
-        li.innerHTML = item;
-        info.unselectedWrapper.appendChild(li);
-        li.addEventListener("click", that.popUnselectedItem, false);
+        var li = doc.createElement("li"),
+          check = doc.createElement("input"),
+          span = doc.createElement("span")
+        ;
+        check.setAttribute("value", item);
+        check.setAttribute("type", "checkbox");
+        span.innerHTML = item;
+        li.appendChild(check);
+        li.appendChild(span);
+        frag.appendChild(li);
+        check.addEventListener("click", function(){
+          that.popUnselectedItem(item);
+        }, false);
       });
+      info.unselectedWrapper.appendChild(frag);
     },
-    createSelectedList: function(){
-      var that = this;
+    createElementSelectedList: function(){
+      var that = this,
+      frag = doc.createDocumentFragment()
+      ;
       info.selectedWrapper.innerHTML = "";
       info.data.selectedItems.forEach(function(item){
-        var li = document.createElement("li");
-        li.innerHTML = item;
-        info.selectedWrapper.appendChild(li);
-        li.addEventListener("click", that.popSelectedItem, false);
+        var li = doc.createElement("li"),
+          check = doc.createElement("input"),
+          span = doc.createElement("span")
+        ;
+        check.setAttribute("value", item);
+        check.setAttribute("checked", "checked");
+        check.setAttribute("type", "checkbox");
+        span.innerHTML = item;
+        li.appendChild(check);
+        li.appendChild(span);
+        frag.appendChild(li);
+        check.addEventListener("click", function(){
+          that.popSelectedItem(item);
+        }, false);
       });
+      info.selectedWrapper.appendChild(frag);
     },
-    popSelectedItem: function() {
-      info.data.selectedItems.splice(info.data.selectedItems.indexOf(this.innerHTML),1);
-      info.data.unselectedItems.push(this.innerHTML);
+    popSelectedItem: function(item) {
+      info.data.selectedItems.splice(info.data.selectedItems.indexOf(item),1);
+      info.data.unselectedItems.push(item);
       local.sort();
       local.updateList();
     },
-    popUnselectedItem: function(){
-      info.data.unselectedItems.splice(info.data.unselectedItems.indexOf(this.innerHTML),1);
-      info.data.selectedItems.push(this.innerHTML);
-      local.sort();
+    popUnselectedItem: function(item){
+      info.data.unselectedItems.splice(info.data.unselectedItems.indexOf(item),1);
+      info.data.selectedItems.push(item);
       local.updateList();
     }
   };
 
   pub = function(data){
-    info.selectedWrapper = document.querySelector("#" + data.selectedId);
-    info.unselectedWrapper = document.querySelector("#" + data.unselectedId);
+    info.selectedWrapper = doc.querySelector("#" + data.selectedId);
+    info.unselectedWrapper = doc.querySelector("#" + data.unselectedId);
+    info.selectUnselectAll = doc.querySelector("#" + data.selectAllId);
+    info.selectUnselectAll.addEventListener("click", local.selectUnselectAll, false);
     info.data = data.data;
-    local.sort();
     local.updateList();
   };
 
